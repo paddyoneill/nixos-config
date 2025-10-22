02f03246217408418f048eeb90b0ab50c91f5f51
Paddy O'Neill <paddy.oneill93@gmail.com>
2025-10-22 12:49:03 +0100

    Update nixvim configuration
    Adds basic configuration options for neovim, and lsp & completion configuration

--- home.nix
+++ home.nix
@@ -86,15 +86,74 @@
   programs.nixvim = {
     enable = true;
 
+    opts = {
+      number = true;
+      relativenumber = true;
+
+      softtabstop = -1;
+      shiftwidth = 0;
+      tabstop = 2;
+      expandtab = true;
+
+      autoindent = true;
+
+      signcolumn = "yes";
+
+      winborder = "rounded";
+    };
+
+    globals.mapleader = "<space>";
+
     colorschemes.gruvbox.enable = true;
+
     plugins.lualine.enable = true;
 
+    plugins.treesitter = {
+      enable = true;
+
+      settings = {
+        indent.enable = true;
+        highlight.enable = true;
+      };
+
+      grammarPackages = pkgs.vimPlugins.nvim-treesitter.allGrammars;
+    };
+
+
+    plugins.blink-cmp = {
+      enable = true;
+      setupLspCapabilities = true;
+
+      settings = {
+        keymap = {
+          preset = "default";
+        };
+        appearance = {
+          nerd_font_variant = "mono";
+          use_nvim_cmp_as_default = true;
+        };
+        signature = {
+          enabled = true;
+        };
+      };
+    };
+
+    plugins.lsp = {
+      enable = true;
+
+      servers = {
+        gopls.enable = true;
+        lua_ls.enable = true;
+        nixd.enable = true;
+      };
+    };
+
     extraConfigLua = ''
-    vim.cmd [[
+      vim.cmd [[
       highlight Normal guibg=none ctermbg=none
-      highlight NonText guibg=none ctermbg=none
-    ]]
-    '';
+        highlight NonText guibg=none ctermbg=none
+      ]]
+      '';
   };
 
   programs.nushell = {
